7dd7c5780abb97c5b37e0e2a80eaff6de6ff61ba
EC2 Default User <ec2-user@ip-172-31-24-65.ap-northeast-1.compute.internal>
2023-04-19 10:17:36 +0000

    [totyubreak]kadai3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
+  resources :users,only:[:index, :show, :edit]
+  resources :books
+  patch 'users/:id' =>'users#update',as:'update_user'
+  get 'home/about'=>'homes#about',as:'about'
+  get 'homes_index'=>'homes#index',as:'home'
+  delete '/books'=>'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
